ce356c45b27ca96aaeade81b051c352c2e5ffab4
Carmen <91155317+CarmenUZH@users.noreply.github.com>
2022-06-27 16:29:39 +0200

    Projektdateien hinzufügen.

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Wichtig: Whitespace ist semi wichtig: klammern müssen klar sein
+using System;
+namespace GradeBook
+    {
+    class Program
+    {
+    static void Main(string[] args) 
+        { 
+            Console.WriteLine("Hello, " + args[0] + "!");
+            Console.WriteLine($"Goodbye, {args[1]}!");
+    }
+    }
+}
