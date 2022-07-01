0b1875359ed5371b348f3b070f859e17a50e402c
Carmen <91155317+CarmenUZH@users.noreply.github.com>
2022-07-01 08:04:38 +0200

    Update Delegate.cs

--- src/GradeBook/Delegate.cs
+++ src/GradeBook/Delegate.cs
@@ -6,6 +6,6 @@ namespace GradeBook
     public delegate void GradeAddedDelegate(object sender, EventArgs args);
     public class Delegate{
     
-//See rest of code on his thing, getting errors i dont unserstand
+//See rest of code on his thing, getting errors i dont understand
 }
 }
